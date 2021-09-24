c335c824d311450dd18c85e7d06ccb4414356721
TimelordRi <35120358+TimelordRi@users.noreply.github.com>
2021-09-24 18:16:49 +0800

    Update run_docred.sh

--- scripts/run_docred.sh
+++ scripts/run_docred.sh
@@ -59,8 +59,8 @@ if true; then
     --num_train_epochs 30.0 \
     --seed 111 \
     --num_class 97 \
-    --save_path ../checkpoint/docred/train_bert-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.pt \
-    --log_dir ../logs/docred/train_bert-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.log
+    --save_path ../checkpoint/docred/train_roberta-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.pt \
+    --log_dir ../logs/docred/train_roberta-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.log
     done
   fi
 fi
