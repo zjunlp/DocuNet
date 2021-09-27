a7db282509c8a597fa3cb2b1d1ac1561f4eddbcc
TimelordRi <35120358+TimelordRi@users.noreply.github.com>
2021-09-27 11:32:44 +0800

    Update run_docred.sh

--- scripts/run_docred.sh
+++ scripts/run_docred.sh
@@ -12,7 +12,7 @@ if true; then
     accum=1
     for ul in ${uls[@]}
     do
-    python -u ../train_balanceloss.py --data_dir ../dataset/docred \
+    python -u ./train_balanceloss.py --data_dir ./dataset/docred \
     --channel_type $channel_type \
     --bert_lr $bl \
     --transformer_type $transformer_type \
@@ -30,8 +30,8 @@ if true; then
     --num_train_epochs 30.0 \
     --seed 66 \
     --num_class 97 \
-    --save_path ../checkpoint/docred/train_bert-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.pt \
-    --log_dir ../logs/docred/train_bert-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.log
+    --save_path ./checkpoint/docred/train_bert-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.pt \
+    --log_dir ./logs/docred/train_bert-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.log
     done
   elif [[ $transformer_type == roberta ]]; then
     type=context-based
@@ -39,9 +39,9 @@ if true; then
     bls=(3e-5)
     ul=4e-4
     accum=2
-    for ul in ${uls[@]}
+    for bl in ${bls[@]}
     do
-    python -u ../train_balanceloss.py --data_dir ../dataset/docred \
+    python -u ./train_balanceloss.py --data_dir ./dataset/docred \
     --channel_type $channel_type \
     --bert_lr $bl \
     --transformer_type $transformer_type \
@@ -59,8 +59,8 @@ if true; then
     --num_train_epochs 30.0 \
     --seed 111 \
     --num_class 97 \
-    --save_path ../checkpoint/docred/train_roberta-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.pt \
-    --log_dir ../logs/docred/train_roberta-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.log
+    --save_path ./checkpoint/docred/train_roberta-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.pt \
+    --log_dir ./logs/docred/train_roberta-lr${bl}_accum${accum}_unet-lr${ul}_type_${channel_type}.log
     done
   fi
 fi
